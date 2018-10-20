6bc2b9d5bff97182f15a20d5b2c17f0aa5b0974b
mariansam <margotka000@gmail.com>
2018-10-20 21:02:14 +0200

    Add some other stuff

--- SpotifyDBus.Tests/UnitTest1.cs
+++ SpotifyDBus.Tests/UnitTest1.cs
@@ -1,18 +1,24 @@
 using NUnit.Framework;
+using SpotifyDBus;
+using System.Threading.Tasks;
 
-namespace Tests
+namespace SpotifyDBus.Tests
 {
     public class Tests
     {
+        Spotify s;
+
         [SetUp]
         public void Setup()
         {
+            s = new Spotify();
         }
 
         [Test]
-        public void Test1()
+        public async Task PauseTest()
         {
-            
+            await s.PauseAsync();
+            Assert.Pass();
         }
     }
 }
--- SpotifyDBus/Spotify.cs
+++ SpotifyDBus/Spotify.cs
@@ -1,9 +1,22 @@
 ﻿using System;
+using spotify.DBus;
+using Tmds.DBus;
+using System.Threading.Tasks;
 
 namespace SpotifyDBus
 {
     public class Spotify
     {
-        
+        IPlayer proxy;
+
+        public Spotify()
+        {
+            proxy = Connection.Session.CreateProxy<IPlayer>("org.mpris.MediaPlayer2.spotify", "/org/mpris/MediaPlayer2");
+        }
+
+        public async Task PauseAsync()
+        {
+            await proxy.PauseAsync();
+        }
     }
 }
