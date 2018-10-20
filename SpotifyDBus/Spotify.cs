c6ae66524f00d5edffee41d9267167d4cae96e30
mariansam <margotka000@gmail.com>
2018-10-20 21:03:09 +0200

    Once more, I forgot to save it

--- SpotifyDBus/Spotify.cs
+++ SpotifyDBus/Spotify.cs
@@ -14,9 +14,16 @@ namespace SpotifyDBus
             proxy = Connection.Session.CreateProxy<IPlayer>("org.mpris.MediaPlayer2.spotify", "/org/mpris/MediaPlayer2");
         }
 
-        public async Task PauseAsync()
-        {
-            await proxy.PauseAsync();
-        }
+        public async Task PauseAsync() => await proxy.PauseAsync();
+
+        public async Task PlayAsync() => await proxy.PlayAsync();
+
+        public async Task PlayPauseAsync() => await proxy.PlayPauseAsync();
+
+        public async Task PreviousAsync() => await proxy.PreviousAsync();
+
+        public async Task NextAsync() => await proxy.NextAsync();
+
+        
     }
 }
