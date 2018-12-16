d6e64f55c1114e786ad3a8a018afb02a67caf2c6
mariansam <margotka000@gmail.com>
2018-12-16 17:15:35 +0100

    Add inline docs to Spotify.cs

--- SpotifyDBus/Spotify.cs
+++ SpotifyDBus/Spotify.cs
@@ -9,26 +9,53 @@ namespace SpotifyDBus
     {
         IPlayer proxy;
 
+        /// <summary>
+        /// Creates a new instance of Spotify.
+        /// </summary>
         public Spotify() : this("org.mpris.MediaPlayer2.spotify")
         { }
 
+        /// <summary>
+        /// Creates a new instance of a mpris-compatible player.
+        /// </summary>
+        /// <param name="service">Service name. eg. org.mpris.MediaPlayer2.spotify</param>
         public Spotify(string service)
         {
             proxy = Connection.Session.CreateProxy<IPlayer>(service, "/org/mpris/MediaPlayer2");
         }
 
+        /// <summary>
+        /// Pauses playback.
+        /// </summary>
         public async Task PauseAsync() => await proxy.PauseAsync();
 
+        /// <summary>
+        /// Starts or resumes playback.
+        /// </summary>
         public async Task PlayAsync() => await proxy.PlayAsync();
 
+        /// <summary>
+        /// Toggles between pause and play.
+        /// </summary>
         public async Task PlayPauseAsync() => await proxy.PlayPauseAsync();
 
+        /// <summary>
+        /// Skips to the previous track in the tracklist.
+        /// </summary>
         public async Task PreviousAsync() => await proxy.PreviousAsync();
 
+        /// <summary>
+        /// Skips to the next track in the tracklist.
+        /// </summary>
         public async Task NextAsync() => await proxy.NextAsync();
 
+        /// <summary>
+        /// Seeks forward in the current track by the specified number of microseconds.
+        /// </summary>
+        /// <param name="offset">The number of microseconds to seek forward. A negative value seeks back.</param>
         public async Task SeekAsync(long offset) => await proxy.SeekAsync(offset);
 
+        // TODO: inline docs
         public double Volume
         {
             get => proxy.GetVolumeAsync().Result;
@@ -36,6 +63,9 @@ namespace SpotifyDBus
             set => proxy.SetVolumeAsync(value);
         }
 
+        /// <summary>
+        /// Indicates that the playback is shuffling.
+        /// </summary>
         public bool Shuffle
         {
             get => proxy.GetShuffleAsync().Result;
@@ -43,6 +73,9 @@ namespace SpotifyDBus
             set => proxy.SetShuffleAsync(value);
         }
 
+        /// <summary>
+        /// The current <see cref="PlaybackStatus" />
+        /// </summary>
         public PlaybackStatus PlaybackStatus
         {
             get
@@ -58,6 +91,9 @@ namespace SpotifyDBus
             }
         }
 
+        /// <summary>
+        /// The current <see cref="LoopStatus" />
+        /// </summary>
         public LoopStatus LoopStatus
         {
             get
@@ -75,6 +111,10 @@ namespace SpotifyDBus
             set => proxy.SetLoopStatusAsync(value.ToString());
         }
 
+        /// <summary>
+        /// The currently playing <see cref="Track" />
+        /// </summary>
+        /// <value></value>
         public Track Track
         {
             get => new Track(proxy.GetMetadataAsync().Result);
